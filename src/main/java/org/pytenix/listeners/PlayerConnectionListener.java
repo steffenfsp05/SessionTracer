5d05f77cef98ce6753e651f1cdf336f5db270b64
BABABABA <steffen.flaskamp@bk-beckum.eu>
2026-05-07 22:23:30 +0200

    added precheck for duplicate qouting

--- src/main/java/org/pytenix/listeners/PlayerConnectionListener.java
+++ src/main/java/org/pytenix/listeners/PlayerConnectionListener.java
@@ -49,6 +49,8 @@ public class PlayerConnectionListener implements Listener {
             if (uuids != null && uuids.contains(uuid))
                 knownAccounts--;
 
+            uuids.remove(uuid);
+
             if (knownAccounts > 0) {
                 DuplicateSessionEvent duplicateEvent = new DuplicateSessionEvent(
                         event.getName(),
